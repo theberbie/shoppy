4c964101d29cabf073ab621e145115c8abdc0e9e
vagrant <vagrant@precise32.(none)>
2016-05-24 05:18:58 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Shoppy::Application.config.secret_key_base = '3a6ecad96b8b426a324f76b4ab3993f098dfb36013a9365481f1848b81eda890756c5669dd14aae68485f2d7dd4932d98d79be7efdff8e1b3d692632fe537367'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Shoppy::Application.config.session_store :cookie_store, key: '_shoppy_session'
